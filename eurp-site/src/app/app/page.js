4dc8ebfa1abc5ca274010d1dd60acc0ab3297b01
Pedro Ferreira <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2025-11-13 23:03:39 +0000

    Update page.js

--- eurp-site/src/app/app/page.js
+++ eurp-site/src/app/app/page.js
@@ -137,8 +137,8 @@ export default function Trade() {
 
         const validTx = await eurc.approve(CONVERTER_CTR, new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 6)).toString());
 
-        await validTx.wait().then(async (e) =>
-        await dai.issueEURP(address,new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 6)).toString());
+        await validTx.wait().then(async (e) =>{
+        await dai.issueEURP(address,new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 6)).toString());}
 
     );
         const dai = new ethers.Contract(CONVERTER_CTR, ABI, signer)
@@ -161,9 +161,9 @@ export default function Trade() {
         const eurp = new ethers.Contract(EURP_CTR, ERC20_ABI, signer);
 
        const validTx = await eurp.approve(CONVERTER_CTR, new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 18)).toString() );
-        await validTx.wait().then(async (e) =>
-            await dai.burnEURP(address,new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 6)).toString() );
-
+        await validTx.wait().then(async (e) => {
+            await dai.burnEURP(address, new BigNumber(amountNotRefined2).multipliedBy(Math.pow(10, 6)).toString());
+        }
     );
 
 
